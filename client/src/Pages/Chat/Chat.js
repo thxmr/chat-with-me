3850770075ef758df2bc71dc52c307864a31f67f
thxmr <trohymas@gmail.com>
2023-03-04 15:00:54 +0100

    enhance/added chat code

--- client/src/Pages/Chat/Chat.js
+++ client/src/Pages/Chat/Chat.js
@@ -1,77 +1,176 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh'
+const conversationContainerStyle = {
+    overflowY: 'scroll',
 }
 
 const conversationStyle = {
     border: '1px solid black',
-    width: '30vw',
-    height: '30vw',
-    overflowY: 'scroll',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'end',
 }
 
-function Chat(){
+function Chat({ socket }){
     const location = useLocation();
-    const { pseudo } = location.state
-    
-    const [ conversation, setConversation ] = useState('');
+    const navigate = useNavigate();
 
-    const getConversation = () => {
-        const conv = document.getElementById('conversation');
-        fetch(process.env.REACT_APP_PROXY+'/api/getConversation/')
+    const [ conversation, setConversation ] = useState('');
+    const [ pseudo, setPseudo ] = useState('');
+    const [ loaded, setLoaded ] = useState(false);
+    const [ isWriting, setIsWriting] = useState([]);
+    const [ users, setUsers] = useState([]);
+
+    const getConversation = async () => {
+        const conv = document.querySelector('#conversationContainer');
+        await fetch(process.env.REACT_APP_PROXY+'/api/getConversation/')
         .then((res) => res.json())  
         .then((res) => {
             if(res.conversation){
                 setConversation(res.conversation)
+                //To set the scrollong position to the bottom by default. Not handled by default because the container is a flex container.
                 conv.innerText = conversation
                 conv.scrollTop = conv.scrollHeight
-            }else{
-                conv.innerText = conversation
             }
         })
     }
 
-    const deleteConversation = () => {
-        fetch(process.env.REACT_APP_PROXY+'/api/deleteConversation/')
+    const getUsers = async () => {
+        await fetch(process.env.REACT_APP_PROXY+'/api/getAllUserLogged/')
         .then((res) => res.json())
-        setConversation('')
-        getConversation()
+        .then((res) => {
+            if(res.users !== users){        //if there is another user logged/disconnected
+                setUsers(res.users)
+            }
+        })
     }
 
-    const sendMessage = () => {
+    const sendMessage = async () => {
         const textArea = document.querySelector('textarea');
-        fetch(process.env.REACT_APP_PROXY+'/api/sendMessage/?' + new URLSearchParams({message: textArea.value, sender: pseudo}))
-        .then((res) => res.json())
+        await fetch(process.env.REACT_APP_PROXY+'/api/sendMessage/?' + new URLSearchParams({message: textArea.value, sender: pseudo}))
+        socket.emit('message', {
+            text: textArea.value,
+            name: pseudo,
+            id: `${socket.id}${Math.random()}`,
+            socketID: socket.id,
+        });
         textArea.value = ''
-        getConversation()
+        isTextAreaEmpty()
     }
     
+    const isTextAreaEmpty = () => {
+        const textArea = document.querySelector('textarea');
+        const button = document.querySelector('button');
+        if(textArea.value === '')
+            {
+                button.setAttribute('disabled','')
+                socket.emit('isnotWriting', {
+                    pseudo: pseudo,
+                    socketID: socket.id,
+                })
+            }else{
+                button.removeAttribute('disabled')
+                button.addEventListener('click',sendMessage)
+                socket.emit('isWriting', {
+                    pseudo: pseudo,
+                    socketID: socket.id
+                })
+            }
+    }
+
+    const generateTypingMessage = () => {
+        const usersDisplayable = 3      //limit of writing users displayable at the time
+        let typingMessage = ''
+        let tempIsWriting = isWriting
+
+        tempIsWriting = tempIsWriting.filter((user) => user.socketID !== socket.id) //remove current user if he is also typing
+        if(tempIsWriting.length > 1){
+            typingMessage = tempIsWriting[0].pseudo
+            tempIsWriting = tempIsWriting.slice(0,usersDisplayable)             //slice the array to keep the exact number of user displayable
+            for(let i=1;i<tempIsWriting.length;i++){
+                tempIsWriting.length - i == 2                                   // if there is at least 2 others players to display after this one
+                    ?
+                        typingMessage += ', ' + tempIsWriting[i].pseudo
+                    :
+                        typingMessage += ' and ' + tempIsWriting[i].pseudo
+            }
+            typingMessage += ' are writing'
+        }else{
+            if(tempIsWriting.length > 0)
+            {
+                typingMessage = tempIsWriting[0].pseudo + ' is writing'
+            }
+        }
+        return typingMessage
+    }
+
     useEffect(() => {
+        if(!location.state){        //used to check if the user tried to access /chat without entering his pseudo
+            navigate('/')
+        }else{
+            setPseudo(location.state.pseudo)
+        }
+
+        socket.on('messageResponse', (data) => {
+            setConversation(data.text)
+        })
+        
+        socket.on('users', (data) => {
+            setUsers(data)
+        })
+
+        socket.on('userWriting', (data) => {
+            setIsWriting(data)
+        })
+
+        getUsers()
         getConversation()
-    }, [conversation]);
+
+        if(users && conversation){
+            setLoaded(true)
+        }
+
+        // eslint-disable-next-line
+    }, [conversation, isWriting]);
 
     return(
-        <>
-            <div style={containerStyle}>
-                <div>
-                    <p>Logged as : {pseudo}</p>
-                    <div id='conversation' style={conversationStyle}/>
-                    <label>Message</label>
-                    <textarea style={{width: '100%', height: '150px', resize: 'none'}}/>
-                    <button onClick={sendMessage}>Send</button>
-                    <button onClick={deleteConversation}>Delete Discussion</button>
-                    </div>
+        <div className='uk-flex uk-flex-center uk-flex-middle uk-background-secondary uk-height-1-1'>
+            <div className="uk-position-center-left uk-margin-left">
+                <p>Users connected</p>
+                <ul>
+                    {
+                        loaded 
+                            ? 
+                                users.map((user, index) => {
+                                    return <li key={index}>{user.pseudo}</li>
+                                })
+                            : 
+                                <span uk-spinner="ratio: 2.5"/>                 //display a loading spinner
+                    }
+                </ul>
+            </div>
+            <div className='uk-width-1-3' style={{minWidth: '300px'}}>
+                <p>Logged as : {pseudo}</p>
+                <div id='conversation' style={conversationStyle} className='uk-flex uk-flex-column uk-width-1-1 uk-height-large'>
+                    <div id='conversationContainer' style={conversationContainerStyle} className='uk-flex'/>
+                    {
+                        !loaded && <div className='uk-flex uk-flex-center uk-flex-middle uk-height-1-1'><span uk-spinner="ratio: 4.5"/></div>  //display a loading spinner inside a container
+                    }    
                 </div>
-        </>
+                <span id='isWriting' style={{position: 'absolute'}}>
+                    {
+                        isWriting.length !== 0 
+                            ? 
+                                generateTypingMessage()
+                            : 
+                                ''
+                    }
+                </span>
+                <div className='uk-margin'>
+                    <label htmlFor='message'>Message</label>
+                    <textarea name='message' onChange={isTextAreaEmpty} className='uk-width-1-1 uk-background-secondary' style={{height: '150px', resize: 'none'}}/>
+                </div>
+                <button className='uk-button uk-button-default' disabled>Send</button>
+            </div>
+        </div>
     )
 }
 export default Chat;
